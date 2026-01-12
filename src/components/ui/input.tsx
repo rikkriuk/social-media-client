103ad2f415cdffa4db12c4800c1a8ee4adb65191
ikokywu <rikiridwan949@gmail.com>
2026-01-12 18:50:05 +0700

    style: adjust status ui input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -2,7 +2,21 @@ import * as React from "react";
 
 import { cn } from "./utils";
 
-const Input = ({ className, type, ...props }: React.ComponentProps<"input">) => {
+type InputProps = React.ComponentProps<"input"> & {
+  inputType?: "danger" | "normal";
+};
+
+const Input = ({ className, type, inputType, ...props }: InputProps) => {
+  const getInputTypeClasses = () => {
+    switch (inputType) {
+      case "danger":
+        return "border-red-500";
+      case "normal":
+      default:
+        return "";
+    }
+  };
+
   return (
     <input
       type={type}
@@ -11,6 +25,7 @@ const Input = ({ className, type, ...props }: React.ComponentProps<"input">) => 
         "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border px-3 py-1 text-base bg-input-background transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
         "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+        getInputTypeClasses(),
         className,
       )}
       {...props}

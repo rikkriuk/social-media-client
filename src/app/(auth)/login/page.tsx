927ff25e857d0b32eb5d5f043b04da0d769c0c4b
ikokywu <rikiridwan949@gmail.com>
2026-01-13 20:57:36 +0700

    refactor: adjust login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,9 +2,14 @@
 
 import { useState } from 'react';
 import Auth from '@/components/Auth';
+import { useTranslationCustom } from '@/i18n/client';
+import useLanguage from '@/zustand/useLanguage';
 
 export default function LoginPage() {
   const [isRegister, setIsRegister] = useState(false);
+
+  const { lng } = useLanguage();
+  const { t } = useTranslationCustom(lng, "auth");
 
   const [fullname, setFullname] = useState('');
   const [email, setEmail] = useState('');
@@ -50,7 +55,8 @@ export default function LoginPage() {
     <section className="min-h-screen bg-gradient-to-br from-blue-50 via-white to-purple-50 dark:from-gray-900 dark:via-gray-900 dark:to-gray-800 flex items-center justify-center">
       <Auth
         isRegister={false}
-        onLogin={() => setIsRegister(false)}
+        onSubmit={() => setIsRegister(false)}
+        t={t}
       />
     </section>
   );

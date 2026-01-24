d4070d5468d8244483263e0f5038b8cbe245b60f
ikokywu <rikiridwan949@gmail.com>
2026-01-24 21:46:31 +0700

    refactor: login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,56 +1,7 @@
 'use client';
 
-import { useState } from 'react';
-import Auth from '@/components/Auth';
-import { useTranslationCustom } from '@/i18n/client';
-import useLanguage from '@/zustand/useLanguage';
-import { webRequest } from '@/helpers/api';
-import { toast } from 'sonner';
-import { useRouter } from 'next/navigation';
-import { ApiError } from '@/types/api';
-import useAuth from '@/zustand/useAuth';
+import LoginScreen from '@/screens/auth/LoginScreen';
 
 export default function LoginPage() {
-  const { lng } = useLanguage();
-  const { t } = useTranslationCustom(lng, "auth");
-  const { doLogin } = useAuth();
-
-  const [loading, setLoading] = useState(false);
-  const router = useRouter();
-
-  const handleSubmit = async (data: Record<string, string>) => {
-    setLoading(true);
-
-    try {
-      const { email, password } = data;
-      const body: any = { 
-        identifierType: "Email",
-        identifier: email,
-        password
-      };
-
-      await doLogin(body, t);
-
-      toast.success(t("login.successLogin"));
-      window.location.href = "/";
-    } catch (err: unknown) {
-      const error = err as ApiError;
-
-      toast.error(error.data?.message || t("invalidCode"));
-      console.error("OTP verify error:", err);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  return (
-    <section className="min-h-screen bg-gradient-to-br from-blue-50 via-white to-purple-50 dark:from-gray-900 dark:via-gray-900 dark:to-gray-800 flex items-center justify-center">
-      <Auth
-        isRegister={false}
-        loading={loading}
-        onSubmit={handleSubmit}
-        t={t}
-      />
-    </section>
-  );
+  return <LoginScreen />;
 }

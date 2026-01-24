51ef85131cb31a4f30e730f77c66178161707f23
ikokywu <rikiridwan949@gmail.com>
2026-01-24 21:45:27 +0700

    feat: add use login hook

---
+++ src/screens/auth/hooks/useLogin.ts
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { ApiError } from "@/types/api";
+import useAuth from "@/zustand/useAuth";
+
+interface UseLoginProps {
+   tFunc: (key: string) => string;
+}
+
+export const useLogin = ({ tFunc }: UseLoginProps) => {
+   const [loading, setLoading] = useState(false);
+   const router = useRouter();
+   const { doLogin } = useAuth();
+
+   const handleSubmit = async (data: Record<string, string>) => {
+      setLoading(true);
+
+      try {
+         const { email, password } = data;
+         const body: any = {
+            identifierType: "Email",
+            identifier: email,
+            password,
+         };
+
+         await doLogin(body, tFunc);
+
+         toast.success(tFunc("login.successLogin"));
+         window.location.href = "/";
+      } catch (err: unknown) {
+         const error = err as ApiError;
+
+         toast.error(error.data?.message || tFunc("invalidCode"));
+         console.error("Login error:", err);
+      } finally {
+         setLoading(false);
+      }
+   };
+
+   return {
+      loading,
+      handleSubmit,
+   };
+};

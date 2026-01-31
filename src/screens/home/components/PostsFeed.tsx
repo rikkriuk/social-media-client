093d644959104ffdf9eed58ea198060ffedfe48b
ikokywu <rikiridwan949@gmail.com>
2026-01-31 21:31:27 +0700

    refactor: adjust share

--- src/screens/home/components/PostsFeed.tsx
+++ src/screens/home/components/PostsFeed.tsx
@@ -1,4 +1,5 @@
 import { PostCard } from "@/components/PostCard";
+import { useSharePost } from "@/hooks/useSharePost";
 import { PostsFeedProps } from "@/types/home";
 
 export const PostsFeed = ({
@@ -9,6 +10,8 @@ export const PostsFeed = ({
    onLikeChange,
    tFunc,
 }: PostsFeedProps) => {
+   const { sharePost } = useSharePost(tFunc);
+
    return (
       <div className="space-y-6">
          {posts.length > 0 ? (
@@ -24,12 +27,18 @@ export const PostsFeed = ({
                   }}
                   content={post.content}
                   image={post.image}
+                  images={
+                     Array.isArray(post.mediaIds) && post.mediaIds.length > 0
+                        ? post.mediaIds.map((id: string) => `${process.env.NEXT_PUBLIC_API_BASE_URL || ""}/uploads/${id}`)
+                        : undefined
+                  }
                   likes={post.likesCount}
                   comments={post.commentsCount}
                   shares={post.sharesCount}
                   initialIsLiked={post.isLiked}
                   onViewDetails={() => onViewDetails?.(post)}
                   onLikeChange={onLikeChange}
+                  onShare={() => sharePost(post.id)}
                />
             ))
             ) : (
